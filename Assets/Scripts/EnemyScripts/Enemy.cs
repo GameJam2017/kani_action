5349d997b11f36d50478710a4289c00406e30751
TSITCL\s162211 <takumi.watabe.program@gmail.com>
2017-04-07 15:22:44 +0900

    エネミーのランダム移動

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 敵
+public class Enemy : Token
+{
+    /// 開始
+    void Start()
+    {
+        // ランダムな方向に移動する
+        // 方向をランダムに決める
+        float dir = Random.Range(0, 359);
+        // 速さは2
+        float spd = 2;
+        SetVelocity(dir, spd);
+    }
+}
