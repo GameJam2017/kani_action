94bdd800a960d7efb3f0ba167ba8d7b8861db836
ShotaroSugiura <asugiura10106@gmail.com>
2017-04-08 17:12:33 +0900

    ステージ用の素材の追加。ウイルスと道具を判断できるようした。

---
+++ Assets/Scripts/Stage/OnCollisionEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnter : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (gameObject.transform.tag == "virus")
+        {
+            Debug.Log(coll.transform.position);
+        }
+    }
+    
+}
