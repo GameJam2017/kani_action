b150548c7ed60776c98425ad0834276771cf8e81
moyoya <hubuki008@gmail.com>
2017-04-10 13:41:35 +0900

    キズにアイテム、エネミーが当たるとゲージが増減する

--- Assets/Scripts/Gauge/GaugeCtrl.cs
+++ Assets/Scripts/Gauge/GaugeCtrl.cs
@@ -13,31 +13,42 @@ public class GaugeCtrl : MonoBehaviour
         _slider = GameObject.Find("Slider").GetComponent<Slider>();
     }
 
-    float _gauge = 0;
+    public float _gauge = 50;
 
     void Update()
     {
-       // gauge上昇
-       if (Input.GetKey(KeyCode.Z))
-       {
-            //maxよりちいさければ
-           if (_gauge < _slider.maxValue)
-           {
-               _gauge += 1;
-           }
-       }
+        // HPゲージに値を設定
+        _slider.value = _gauge;
+    }
 
-       // gauge減少
-       if (Input.GetKey(KeyCode.X))
-       {
-            //minより大きければ
-           if (_gauge > _slider.minValue)
-           {
-               _gauge += -1;
-           }
-       }
-   
+    //エネミーとキズが当たったときに呼び出される
+    void onEnemy(int damage)
+    {
+        //minより大きければ
+        if (_gauge - damage > _slider.minValue)
+        {
+            _gauge -= damage;
+        }
+        else
+        {
+            _gauge = 0;
+        }
+        // HPゲージに値を設定
+        _slider.value = _gauge;
+    }
 
+    //エネミーとキズが当たったときに呼び出される
+    void onItem(int cure)
+    {
+        //minより大きければ
+        if (_gauge + cure > _slider.minValue)
+        {
+            _gauge += cure;
+        }
+        else
+        {
+            _gauge = 100;
+        }
         // HPゲージに値を設定
         _slider.value = _gauge;
     }
